0f861de30f72a30ea5bbb79680565413f91f1df2
Abhay Singh <78447760+Abhaysingh77@users.noreply.github.com>
2025-03-17 16:24:00 +0530

    fixed some responsiveness

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ez-works-assignment
+# ezworks-assignment
+**Live Project URL**: https://ezworks-assignment.vercel.app/ 
+
+
+## Getting Started
+To start up the project, follow these steps:
+
+1. **Clone the repository**:
+    ```sh
+    git clone https://github.com/Abhaysingh77/ezworks-assignment.git
+    ```
+2. **Navigate to the project directory**:
+    ```sh
+    cd ezworks-assignment
+    ```
+3. **Install dependencies**:
+    ```sh
+    npm install
+    ```
+4. **Start the development server**:
+    ```sh
+    npm run dev
+    ```
+
--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import Hero from "./components/Hero";
 import CardContainer from "./components/CardContainer";
 import Contact from "./components/Contact";
-import './App.css' 
+import "./App.css";
+import useWindowWidth from "./hooks/useWindowWidth";
 
 const App = () => {
+  const width = useWindowWidth();
+  console.log(width)
   return (
     <div className="container">
       <div className="left-section">
         <Hero />
-        <Contact />
+        {width > 768 && <Contact />}
       </div>
-      <CardContainer />
+      <div className="right-section">
+        <CardContainer />
+      </div>
+      {width <= 768 && <Contact />}
     </div>
   );
 };
